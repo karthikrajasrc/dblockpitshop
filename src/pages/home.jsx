7cf15ae8495aac8a1c1906dd4ef2a09f6429a4c2
karthikrajasrc <karthikrajasrc@gmail.com>
2026-02-08 21:05:44 +0530

    added animation images to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,19 @@ import logo from "../assets/logo.webp";
 import Beams from '../components/background';
 import Scroll from "../components/scroll"; 
 import bikesvg from "../assets/bikeimgsvg.svg"
+import smk from "../assets/smkhelmet.svg"
+import shoie from "../assets/shoeihelmet.svg"
+import agv from "../assets/agvhelmet.svg"
+import axxis from "../assets/axxishelmet.svg"
+import akrapovic from "../assets/akrapovic.svg"
+import sc from "../assets/sc.svg"
+import termi from "../assets/termi.svg"
+import yosi from "../assets/yosi.svg"
+import castrol from "../assets/castrol.svg"
+import shell from "../assets/shell.svg"
+import motul from "../assets/motul.svg"
+import mobil from "../assets/mobil.svg"
+
 
 function Home() {
   return (
@@ -54,10 +67,30 @@ function Home() {
               </div>
             <div className="main-list">
               <ul>
-                <li><strong>Helmets:</strong> Essential safety gear designed for impact protection and aerodynamics, with top brands including SMK, Shoei, Arai, and AGV.</li>
-                <li><strong>Racing Exhausts:</strong> Engineered to improve airflow, reduce weight, and boost horsepower; industry leaders include Akrapovič, SC-Project, Termignoni, and Yoshimura.</li>
+                <li><strong>Helmets:</strong> Essential safety gear designed for impact protection and aerodynamics, with top brands including SMK, Shoei, Arai, and AGV.
+                <div className="helmet-scroll">
+                   <div className="helmet-track">
+                      <img src={smk} alt="smkhelmet" /><img src={shoie} alt="shoiehelmet" /><img src={agv} alt="agvhelmet" /><img src={axxis} alt="axxishelmet" />
+                      <div className="duplicate-set">
+                      <img src={smk} alt="smkhelmet" /><img src={shoie} alt="shoiehelmet" /><img src={agv} alt="agvhelmet" /><img src={axxis} alt="axxishelmet" /></div>
+                  </div></div></li>
+                <li><strong>Racing Exhausts:</strong> Engineered to improve airflow, reduce weight, and boost horsepower; industry leaders include Akrapovic, SC-Project, Termignoni, and Yoshimura.
+                                <div className="helmet-scroll">
+                   <div className="helmet-track">
+                      <img src={sc} alt="sc" /><img src={akrapovic} alt="akrapovic" /><img src={termi} alt="termi" /><img src={yosi} alt="yosi" />
+                      <div className="duplicate-set">
+                      <img src={sc} alt="sc" /><img src={akrapovic} alt="akrapovic" /><img src={termi} alt="termi" /><img src={yosi} alt="yosi" /></div>
+                  </div></div>
+                </li>
                 <li><strong>Brake Discs: </strong>High-performance rotors that provide superior heat dissipation and stopping power, prominently manufactured by Brembo, Galfer, and EBC.</li>
-                <li><strong>Engine Oil:</strong> Specialized lubricants that reduce internal friction and protect high-revving engines, with premium options from Motul, Castrol, Shell, and Liqui Moly.</li>
+                <li><strong>Engine Oil:</strong> Specialized lubricants that reduce internal friction and protect high-revving engines, with premium options from Motul, Castrol, Shell, and mobil.
+                      <div className="helmet-scroll">
+                   <div className="helmet-track">
+                      <img src={castrol} alt="castrol" /><img src={shell} alt="shell" /><img src={motul} alt="motul" /><img src={mobil} alt="mobil" />
+                      <div className="duplicate-set">
+                      <img src={castrol} alt="castrol" /><img src={shell} alt="shell" /><img src={motul} alt="motul" /><img src={mobil} alt="mobil" /></div>
+                  </div></div>
+                </li>
                 <li><strong>Performance Air Filters</strong>: High-flow filters that allow more oxygen into the engine for better combustion, often branded by K&N, DNA Performance, and BMC.</li>
               </ul>
           </div>
